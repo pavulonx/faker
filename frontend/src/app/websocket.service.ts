d052ac58dcc24b5bc3415b9429576e0838e5e24c
Jakub Rozenbajger <thequetran@gmail.com>
2018-11-03 16:15:05 +0100

    update websocket service

--- frontend/src/app/websocket.service.ts
+++ frontend/src/app/websocket.service.ts
@@ -1,20 +1,27 @@
 import {Injectable} from '@angular/core';
-import {WebSocketSubject} from "rxjs/observable/dom/WebSocketSubject";
-import {Observable} from "rxjs/Observable";
+import {Observable} from 'rxjs/Observable';
+import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
+import {stringify} from "querystring";
+
 
 @Injectable()
 export class WebsocketService {
 
   private _wsSubject: WebSocketSubject<any>;
-  private wsUrl = 'ws://localhost:8080/notifications/';
+  private wsUrl = 'ws://localhost:8180/notifications/';
   private clientId = 'CHUJ';
 
   constructor() {
   }
 
   public get wsSubject(): WebSocketSubject<any> {
+    console.log("wsSubjectXXXXXXXXXXXXX");
     if (!this._wsSubject || this._wsSubject.closed) {
-      this._wsSubject = WebSocketSubject.create(this.url);
+      this._wsSubject = webSocket({
+          url: this.url,
+          deserializer: this.stringDeserializer()
+        }
+      );
     }
     return this._wsSubject;
   }
@@ -30,6 +37,10 @@ export class WebsocketService {
 
   private get url(): string {
     return this.wsUrl + this.clientId
+  }
+
+  stringDeserializer() {
+    return x => stringify(x);
   }
 
 }
