b4bf0144714888b9b1b2bf435f3623c8e4362d81
JRozen <jakub.rozenbajger@nokia.com>
2018-11-26 18:58:20 +0100

    use one kafka consumer

--- notifier/src/main/scala/cf/jrozen/faker/notifier/NotifierApp.scala
+++ notifier/src/main/scala/cf/jrozen/faker/notifier/NotifierApp.scala
@@ -1,35 +1,54 @@
 package cf.jrozen.faker.notifier
 
-import cats.effect.{Sync, _}
+import cats.data.NonEmptyList
+import cats.effect._
 import cats.implicits._
+import cf.jrozen.faker.kafka.KafkaConfiguration
 import fs2.Stream
+import fs2.concurrent.Topic
+import fs2.kafka.{consumerExecutionContextStream, consumerStream}
+import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.http4s.HttpApp
 import org.http4s.implicits._
 import org.http4s.server.Router
 import org.http4s.server.blaze.BlazeServerBuilder
 
 import scala.concurrent.duration._
+import scala.util.Random
 
 object NotifierApp extends IOApp {
 
   override def run(args: List[String]): IO[ExitCode] = {
     for {
-      conf <- NotifierConfig.load[IO]
+      conf <- Stream.eval(NotifierConfig.load[IO])
 
+      topic <- Stream.eval(Topic[IO, ConsumerRecord[String, String]](new ConsumerRecord("", 0, 0, "START", null)))
       app = Router {
-        "/" -> NotifierEndpoints.endpoints[IO](NotifierService[IO](conf))
+        "/" -> NotifierEndpoints.endpoints[IO](NotifierService[IO](topic))
       }.orNotFound
 
-      server <- server[IO](app).compile.drain.as(ExitCode.Success)
+      server <- kafkaStream[IO](conf).to(topic.publish).concurrently(server[IO](app))
     } yield server
-  }
 
-  def server[F[_] : Sync : ConcurrentEffect : Timer](httpApp: HttpApp[F]): Stream[F, ExitCode] = {
+  }.compile.drain.as(ExitCode.Success)
+
+  def server[F[_] : ConcurrentEffect : Timer](httpApp: HttpApp[F]): Stream[F, ExitCode] = {
     BlazeServerBuilder[F]
       .bindHttp(8280)
       .withWebSockets(true)
-      .withIdleTimeout(1 minute) //todo: send ping responses by ws to avoid idle state issue
+      .withIdleTimeout(1 minute)
       .withHttpApp(httpApp)
       .serve
   }
+
+  def kafkaStream[F[_] : ConcurrentEffect : ContextShift : Timer](notifierConfig: NotifierConfig
+                                                                 ): Stream[F, ConsumerRecord[String, String]] = for {
+    executionContext <- consumerExecutionContextStream[F]
+    notifierGroupId = s"notifier_${Random.alphanumeric.take(5).mkString}"
+    consumerSettings = KafkaConfiguration.consumerSettings[String](notifierGroupId, notifierConfig.kafka)
+    consumer <- consumerStream[F].using(consumerSettings(executionContext))
+    _ <- consumer.subscribe(NonEmptyList.one(notifierConfig.notificationsTopic))
+    message <- consumer.stream
+  } yield message.record
+
 }
--- notifier/src/main/scala/cf/jrozen/faker/notifier/NotifierService.scala
+++ notifier/src/main/scala/cf/jrozen/faker/notifier/NotifierService.scala
@@ -1,37 +1,24 @@
 package cf.jrozen.faker.notifier
 
-import cats.data.NonEmptyList
-import cats.effect.{ConcurrentEffect, ContextShift, Timer}
-import cf.jrozen.faker.kafka.KafkaConfiguration
+import cats.effect._
 import fs2.Stream
-import fs2.kafka._
+import fs2.concurrent.Topic
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.http4s.websocket.WebSocketFrame.Text
 
-import scala.util.Random
+class NotifierService[F[_] : ConcurrentEffect : ContextShift : Timer](val topic: Topic[F, ConsumerRecord[String, String]]) {
 
-class NotifierService[F[_] : ConcurrentEffect : ContextShift : Timer](val notifierConfig: NotifierConfig) {
-
-  def kafkaStream(workspaceId: String): Stream[F, ConsumerRecord[String, String]] = { //todo: one consumer per app
-    {
-      for {
-        executionContext <- consumerExecutionContextStream[F]
-        consumerSettings = KafkaConfiguration.consumerSettings[String](s"notifier_${Random.alphanumeric.take(5).mkString}", notifierConfig.kafka)
-        consumer <- consumerStream[F].using(consumerSettings(executionContext))
-        _ <- consumer.subscribe(NonEmptyList.one(notifierConfig.notificationsTopic))
-        message <- consumer.stream
-      } yield message.record
-    }
-  }
-
-  def subscribe(workspaceId: String) = {
-    kafkaStream(workspaceId).map(msg => Text(msg.value().toString))
+  def subscribe(workspaceId: String): Stream[F, Text] = {
+    topic
+      .subscribe(4)
+      .filter(cr => workspaceId == cr.key())
+      .map(msg => Text(msg.value().toString))
   }
 }
 
 object NotifierService {
 
-  def apply[F[_] : ConcurrentEffect : ContextShift : Timer](notifierConfig: NotifierConfig): NotifierService[F] =
-    new NotifierService[F](notifierConfig)
+  def apply[F[_] : ConcurrentEffect : ContextShift : Timer](topic: Topic[F, ConsumerRecord[String, String]]): NotifierService[F] =
+    new NotifierService[F](topic)
 
 }
