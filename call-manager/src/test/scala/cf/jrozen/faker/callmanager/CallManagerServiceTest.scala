bb3e9edd34d672f060846136f946e2c05d61a1fc
JRozen <jakub.rozenbajger@nokia.com>
2018-12-26 21:42:03 +0100

    add test class

---
+++ call-manager/src/test/scala/cf/jrozen/faker/callmanager/CallManagerServiceTest.scala
@@ -0,0 +1,17 @@
+package cf.jrozen.faker.callmanager
+
+import cats.effect.IO
+import cf.jrozen.faker.mongo.repository.CallRepository
+import org.scalatest.FunSuite
+
+class CallManagerServiceTest
+  extends FunSuite {
+
+  test("should process add event") {
+
+// todo: kafka tetst
+
+  }
+
+
+}
